bc27d56fc63b8042505ccff8c94b96f7b3b086e7
LY-zzz <cc20000223@126.com>
2026-04-25 12:16:07 +1000

    Add glass press GUI texture

--- src/main/java/com/ly/tfcglassworkshop/client/screen/GlassPressScreen.java
+++ src/main/java/com/ly/tfcglassworkshop/client/screen/GlassPressScreen.java
@@ -1,14 +1,19 @@
 package com.ly.tfcglassworkshop.client.screen;
 
+import com.ly.tfcglassworkshop.TFCGlassWorkshop;
 import com.ly.tfcglassworkshop.menu.GlassPressMenu;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
 
 public class GlassPressScreen extends AbstractContainerScreen<GlassPressMenu> {
+    private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(TFCGlassWorkshop.MOD_ID, "textures/gui/glass_press.png");
     private static final int PROGRESS_BAR_WIDTH = 12;
     private static final int PROGRESS_BAR_HEIGHT = 8;
+    private static final int PROGRESS_BAR_TEXTURE_X = 176;
+    private static final int PROGRESS_BAR_TEXTURE_Y = 0;
 
     public GlassPressScreen(GlassPressMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
@@ -29,30 +34,14 @@ public class GlassPressScreen extends AbstractContainerScreen<GlassPressMenu> {
         int left = leftPos;
         int top = topPos;
 
-        guiGraphics.fill(left, top, left + imageWidth, top + imageHeight, 0xFFC6C6C6);
-        guiGraphics.fill(left + 3, top + 3, left + imageWidth - 3, top + imageHeight - 3, 0xFF8B8B8B);
-        guiGraphics.fill(left + 7, top + 7, left + imageWidth - 7, top + imageHeight - 7, 0xFFC6C6C6);
-
-        renderSlotBackground(guiGraphics, left + 43, top + 34);
-        renderSlotBackground(guiGraphics, left + 79, top + 34);
-        renderSlotBackground(guiGraphics, left + 115, top + 34);
-
-        guiGraphics.hLine(left + 64, left + 75, top + 43, 0xFF404040);
+        guiGraphics.blit(TEXTURE, left, top, 0, 0, imageWidth, imageHeight);
         renderProgressBar(guiGraphics, left + 100, top + 39);
-    }
-
-    private static void renderSlotBackground(GuiGraphics guiGraphics, int x, int y) {
-        guiGraphics.fill(x, y, x + 18, y + 18, 0xFF373737);
-        guiGraphics.fill(x + 1, y + 1, x + 17, y + 17, 0xFFE0E0E0);
     }
 
     private void renderProgressBar(GuiGraphics guiGraphics, int x, int y) {
         int progressWidth = menu.getScaledProgress(PROGRESS_BAR_WIDTH);
-        guiGraphics.fill(x, y, x + PROGRESS_BAR_WIDTH, y + PROGRESS_BAR_HEIGHT, 0xFF3B3B3B);
-        guiGraphics.fill(x + 1, y + 1, x + PROGRESS_BAR_WIDTH - 1, y + PROGRESS_BAR_HEIGHT - 1, 0xFFB0B0B0);
-
         if (progressWidth > 0) {
-            guiGraphics.fill(x + 1, y + 1, x + 1 + progressWidth, y + PROGRESS_BAR_HEIGHT - 1, 0xFFD88A32);
+            guiGraphics.blit(TEXTURE, x, y, PROGRESS_BAR_TEXTURE_X, PROGRESS_BAR_TEXTURE_Y, progressWidth, PROGRESS_BAR_HEIGHT);
         }
     }
 }
